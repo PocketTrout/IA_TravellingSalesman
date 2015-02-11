cc4db3700cc2428aa2f8e89daea044863a245d90
MichaelCaraccio <michael.caraccio@he-arc.ch>
2015-02-11 15:39:43 +0100

    Importation des villes depuis un fichier est maintenant possible

--- geneticalgorithm/caracciobrandt.py
+++ geneticalgorithm/caracciobrandt.py
@@ -1,25 +1,34 @@
-from models.city import City
-
 __author__ = 'kilian.brandtdi'
 
 from models.city import City
 from models.solution import Solution
 from models.point import Point
 from models.problem import Problem
+from models.file import File
+
 
 #TODO write solve function
 def ga_solve(file=None, gui=True, maxtime=0):
     return 0
 
-
 if __name__ == "__main__":
-    city1 = City(Point(2,3),"1")
-    city2 = City(Point(3,4),"2")
-    city3 = City(Point(5,6),"3")
-    problem = Problem([city1,city2,city3])
+    city1 = City(Point(2, 3), "1")
+    city2 = City(Point(3, 4), "2")
+    city3 = City(Point(5, 6), "3")
+    problem = Problem([city1, city2, city3])
     soluce = Solution(problem)
     soluce.addNextCity(city1)
     soluce.addNextCity(city2)
     soluce.addNextCity(city3)
     soluce.addNextCity(city3)
     soluce.displayPath()
+
+    # Importation des villes depuis un fichier
+    file_import = File("../extern/pb010.txt")
+
+    # Stockage des villes dans une liste
+    citiesFromFile = []
+    citiesFromFile = file_import.from_file()
+
+    print(citiesFromFile)
+    print(citiesFromFile[0])
---
+++ models/file.py
@@ -0,0 +1,23 @@
+import csv
+
+from models.city import City
+from models.point import Point
+
+class File:
+
+    def __init__(self, path):
+       self._path = path
+
+    def from_file(self):
+        citylist = []
+        with open(self._path) as f:
+            reader = csv.reader(f, delimiter=" ")
+            d = list(reader)
+
+        for number in d:
+            #print(" %s | %s | %s" % (number[0], number[1], number[2]))
+            citylist.append(City(Point(int(number[1]), int(number[2])), number[0]))
+
+        return citylist
+
+
